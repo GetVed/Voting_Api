3e66b5c5877e410541038bae3a5b00de9ab70179
Ved <vedbhai900@gmail.com>
2025-04-05 23:07:18 +0530

    Vote count added

--- Candidate/candidate.controller.js
+++ Candidate/candidate.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../User/user.schema.js"
 import { Candidate } from "./candidate.schema.js"
+import { ObjectId } from "mongodb";
 
 const checkAdmin=async (userId)=>{
    try{
@@ -37,7 +38,7 @@ export const updateCandidate=async(req,res)=>{
             return res.status(403).json({success:false , error:"User does not have admin role."})
         }
 
-        const response = await Candidate.findOneAndUpdate(candidateId,updateCandidateData,{
+        const response = await Candidate.findOneAndUpdate({_id:new ObjectId(candidateId)},updateCandidateData,{
             new:true,
             runValidators:true
         })
@@ -48,7 +49,7 @@ export const updateCandidate=async(req,res)=>{
 
         res.status(201).json({success:true , error:false , message:"Candidate Updated." , response})
     } catch (err) {
-        res.status(500).json({success:false , error:"Internal Server Error."})
+        res.status(500).json({success:false , error:"Internal Server Error." , message:err.message})
     }
 }
 
@@ -61,7 +62,7 @@ export const deleteCandidate=async(req,res)=>{
         return res.status(403).json({success:false , error:"User does not have admin role."})
        }
 
-       const response = await Candidate.findByIdAndDelete(candidateId)
+       const response = await Candidate.findByIdAndDelete({_id:new ObjectId(candidateId)})
 
        if(!response){
         return res.status(404).json({success:false , error:"Candidate not found..."})
@@ -69,5 +70,61 @@ export const deleteCandidate=async(req,res)=>{
        res.status(200).json({success:true , error:false , message:"Candidate Deleted." , response})
     }catch (err) {
         res.status(500).json({success:false , error:"Internal Server Error."})
+    }
+}
+
+export const addVoter = async(req,res)=>{
+    try {
+        const userId=req.userId
+        const candidateId=req.params.candidateId
+        const candidate = await Candidate.findById({_id:new ObjectId(candidateId)})
+        if(!candidate){
+            return res.status(404).json({success:false , error:"Candidate not found..."})
+        }
+        const user = await User.findById(userId)
+        if(!user){
+            return res.status(404).json({success:false , error:"User not found..."})
+        }
+        if(user.role==='admin'){
+            return res.status(403).json({success:false , error:"Admin not allowed to vote."})
+        }
+        if(user.isVoted){
+            return res.status(400).json({success:false , error:"You already voted..."})
+        }
+
+        candidate.votes.push({user:userId})
+        candidate.voteCount++
+        
+        user.isVoted=true
+        await user.save()
+        await (await candidate.save()).populate('votes.user')
+       
+        res.status(200).json({success:true , error:false , message:"Vote recorded successfully.",candidate})
+    } catch (err) {
+        res.status(500).json({success:false , error:"Internal Server Error."})
+    }
+}
+
+export const voteCount = async(req,res)=>{
+    try {
+        const candidate = await Candidate.find().sort({voteCount:'desc'})
+        const voteRecord = candidate.map((data)=>{
+            return {
+                party:data.party,
+                count:data.voteCount
+            }
+        })
+        return  res.status(200).json({success:true , error:false , voteRecord})
+    } catch (err) {
+        res.status(500).json({success:false , error:"Internal Server Error." , message:err.message})
+    }
+}
+
+export const candidateList = async(req,res)=>{
+    try {
+        const candidates = await Candidate.find({},'name party -_id')
+        res.status(200).json(candidates)
+    } catch (err) {
+        res.status(500).json({success:false , error:"Internal Server Error." , message:err.message}) 
     }
 }
--- Candidate/candidate.rout.js
+++ Candidate/candidate.rout.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addCandidate, updateCandidate ,deleteCandidate} from './candidate.controller.js';
+import { addCandidate, updateCandidate ,deleteCandidate,addVoter, voteCount, candidateList} from './candidate.controller.js';
 import { jwtAuth } from '../middleware/jwt.middleware.js';
 
 const CandidateRouter=express.Router()
@@ -7,4 +7,7 @@ const CandidateRouter=express.Router()
 CandidateRouter.post('/',jwtAuth,addCandidate)
 CandidateRouter.put('/:candidateId',jwtAuth,updateCandidate)
 CandidateRouter.delete('/:candidateId',jwtAuth,deleteCandidate)
+CandidateRouter.get('/vote/count',voteCount)
+CandidateRouter.get('/vote/:candidateId',jwtAuth,addVoter)
+CandidateRouter.get('/',candidateList)
 export default CandidateRouter;
